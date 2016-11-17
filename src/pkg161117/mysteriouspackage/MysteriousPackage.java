069f8cb85a2cddd61c5374626e2d6bf958e93119
evzha0751 <evzha0751@7LLM6Y1.Student.UGDSB.ED>
2016-11-17 13:57:45 -0500

    new commit

--- src/pkg161117/mysteriouspackage/MysteriousPackage.java
+++ src/pkg161117/mysteriouspackage/MysteriousPackage.java
@@ -12,14 +12,29 @@ public class MysteriousPackage {
    
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int classes,ID1,ID2;
+        int classes,ID1,ID2,IDs;
+        ArrayList<Integer> studentList=new ArrayList();
+        ArrayList<Integer> periodList=new ArrayList();
         
         classes=input.nextInt();
+        System.out.println("classes"+classes);
         ID1=input.nextInt();
         ID2=input.nextInt();
+        System.out.println("ID"+ID1+"  "+ID2);
         
-        for (int i=0;i<=classes;i++){
-            int period,students;
+        for (int i=1;i<=classes;i++){
+            int periods,students;
+            periods=input.nextInt();
+            students=input.nextInt();
+            
+            System.out.println("Class"+i+"  period"+periods+"  "+students);
+            for(int x=1;x<=students;i++){
+                IDs=input.nextInt();
+                
+                periodList.add(IDs);
+                studentList.add(periods);
+                System.out.println(x+"IDs"+IDs);
+            }
             
         }
     }
