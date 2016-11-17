440071bc29e579ee03e70016bc69e0bfc1a3ddcb
evzha0751 <evzha0751@7LLM6Y1.Student.UGDSB.ED>
2016-11-17 13:38:14 -0500

    new commit

---
+++ src/pkg161117/mysteriouspackage/MysteriousPackage.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg161117.mysteriouspackage;
+
+import java.util.*;
+public class MysteriousPackage {
+
+   
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int classes,ID1,ID2;
+        
+        classes=input.nextInt();
+        ID1=input.nextInt();
+        ID2=input.nextInt();
+        
+        for (int i=0;i<=classes;i++){
+            int period,students;
+            
+        }
+    }
+    
+}
